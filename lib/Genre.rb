10a4463e6f18b971e6f0f6859762a6052bad9899
Alexander Gould <alexander.gould94@gmail.com>
2021-03-16 16:11:18 -0700

    commit tree 8d4a9218d3fd45749d57589943dc284908e50f78

---
+++ lib/Genre.rb
@@ -0,0 +1,36 @@
+class Genre
+    extend Concerns::Findable
+
+    attr_accessor :name
+
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        self.all.clear
+    end
+
+    def save
+        self.class.all << self
+        self
+    end
+
+    def self.create(name)
+        new(name).save
+    end
+
+    def songs
+        Song.all.select{|s| s.genre == self}
+    end
+
+    def artists
+        songs.map{|song| song.artist}.uniq
+    end
+end
